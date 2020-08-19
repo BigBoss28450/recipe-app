665b6a638615fbde64d6f766ff464f6397547ec8
Alexis Isidoro Bolaños <alexis.isidoro_91@hotmail.com>
2020-08-19 01:12:45 -0500

    Se añadio el archvio App_Env

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { APP_KEY } from "./App_Env";
 
 function App() {
+  const APP_ID = "d5b6156e";
+
+  // const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{APP_KEY}</h1>
     </div>
   );
 }
